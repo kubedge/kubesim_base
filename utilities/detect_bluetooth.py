4e31b4e7afa21c7530c2acf5e20b8ff876caf0aa
Hypriot Pirate <pirate@black-pearl.localdomain>
2018-10-13 16:48:58 +0000

    [hack4easy] - Added utility scripts 1. detect_bluetooth.py => using blues module 2. detect_wifi.py => using arp-scan Last couple of commits did not go thro gerrit and directly pushed to github
    Change-Id: Ia8d0775ea184f1abe35681ba7c24763fe734d915

--- utilities/detect_bluetooth.py
+++ utilities/detect_bluetooth.py
@@ -1,13 +1,13 @@
 import bluetooth, time
 
-print("Bluetooth Detection Demo!")
-
 search_time = 10
 addr = None
-device_names = ['Siraj','Krishna','Aysuh','User1', 'User2']
+# Device names configured on handset for bluetooth discovery
+device_names = ['Siraj','Krishna','Ayush','User1', 'User2']
+device_addrs = []
 
+print("Scanning for bluetooth devices...")
 while True:
-    print("Searching for devices...")
     nearby_devices = bluetooth.discover_devices(duration=search_time, 
                                                 flush_cache=True, 
                                                 lookup_names=True)
@@ -17,12 +17,15 @@ while True:
     else:
         print("No devices Detected!")
 
+    match=False
     for addr, name in nearby_devices:
         if name in device_names:
-            print("Got {} in Boluetooth coverage, " \
+            print("Got {} in Bluetooth coverage, " \
                   "will go sleep for sometime".format(name))
-            time.sleep(20)
-    print("Still waiting for someone from our team to " \
-          "come in coverage area")
-    time.sleep(1)
+            match=True
+    if not match:
+        print("Still waiting for someone from our team to " \
+            "come in bluetooth coverage area")
+    time.sleep(2)
+    print("Re-Scanning...")
 
---
+++ utilities/detect_wifi.py
@@ -0,0 +1,30 @@
+import os
+import time
+import subprocess
+
+device_names = ['Siraj','Krishna','Ayush','RICOH', 'User2']
+device_addrs = []
+command = "sudo arp-scan -l | tail -n +3 | head -n -3 > /tmp/wifi_connected_devices"
+
+print("Scanning for devices connected to WIFI...")
+while True:
+    #I know it is a security issue to use shell=True :)
+    #getting the Popen with complex was tricky. will fix this later
+    subprocess.call(command, shell=True)
+    detected_devices = open('/tmp/wifi_connected_devices', "r")
+
+    with open("/tmp/wifi_connected_devices") as tmpfile:
+         data=tmpfile.readlines()
+
+    match = False
+    for name in device_names:
+        for line in data:
+            if name in line:
+                print("Got {} in Wifi coverage".format(name))
+                match=True
+
+    if not match:
+        print("Still waiting for someone from our team to " \
+            "come in wifi coverage area")
+    time.sleep(5)
+    print("Re-Scanning...")
