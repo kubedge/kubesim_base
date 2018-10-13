41ac64186bcce3bdea01796c85e298a474c0948a
sirajyasin <sirajudeen.yasin@gmail.com>
2018-10-13 17:55:06 +0000

    [hack4easy] updated client code
    Change-Id: I5cd616d61acd0182656cdcc5151febe350b24389

--- grpc/go/kubedge_client/main.go
+++ grpc/go/kubedge_client/main.go
@@ -10,37 +10,55 @@ import (
 )
 
 const (
-        address     = "172.17.0.2:50051"
+	address     = "172.17.0.2:50051"
+        //address     = "localhost:50051"
         defaultNetwork = "Bluetooth"
-        defaultProtocol = "X1000001"
+        S1ProtoHead = "X1000001"
+        SNProtoHead = "XN888888"
+	defaultProtocol =  "S1"
+	defaultDemo = "protocol"
 )
 
 func main() {
-        // Set up a connection to the server.
-        conn, err := grpc.Dial(address, grpc.WithInsecure())
-        if err != nil {
-                log.Fatalf("did not connect: %v", err)
-        }
-        defer conn.Close()
-        c := pb.NewKubedgeClient(conn)
+    // Set up a connection to the server.
+    conn, err := grpc.Dial(address, grpc.WithInsecure())
+    if err != nil {
+            log.Fatalf("did not connect: %v", err)
+    }
+    defer conn.Close()
+    c := pb.NewKubedgeClient(conn)
 
-        // Contact the server and print out its response.
-        network := defaultNetwork
-        if len(os.Args) > 1 {
-                network = os.Args[1]
-        }
-        ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-        defer cancel()
-        r, err := c.FiveGDemo(ctx, &pb.EnodeRequest{Protocol: defaultProtocol})
+    // Contact the server and print out its response.
+    demo := defaultDemo
+    network := defaultNetwork
+    protocol := defaultProtocol
+    protoHead := S1ProtoHead
+    if len(os.Args) > 1 {
+       demo = os.Args[1]
+       if (demo == defaultDemo) {
+           protocol = os.Args[2]
+           if protocol != defaultProtocol {
+               protoHead =  SNProtoHead
+           }
+       } else {
+           network = os.Args[2]
+       }
+    }
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    if demo == defaultDemo {
+        r, err := c.FiveGDemo(ctx, &pb.EnodeRequest{Protocol: protoHead})
         if err != nil {
-                log.Fatalf("Error: %v", err)
+            log.Fatalf("Error: %v", err)
         }
         log.Printf("%s", r.Message)
+    } else {
         rr, errr := c.DetectNW(ctx, &pb.UERequest{Network: network})
         if errr != nil {
-                log.Fatalf("Error: %v", errr)
+            log.Fatalf("Error: %v", errr)
         }
         log.Printf("%s", rr.Message)
+    }
 }
 
 
