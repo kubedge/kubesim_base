f6a2d91478617d9fd74ec4a29cde3d047df25128
Robert Boyd <rb422w@att.com>
2018-10-14 02:53:34 +0000

    [XXXXX] revised commit convert client and server to packages so can import,         client params passed rather than from os args.
    Change-Id: Ic66d6373599bc271c37efa6d849cac51b8452cd9

---
+++ grpc/go/kubedge_client/client.go
@@ -0,0 +1,64 @@
+package client
+
+import (
+        "log"
+        "time"
+        "golang.org/x/net/context"
+        "google.golang.org/grpc"
+	pb  "github.com/kubedge/kubesim_base/grpc/go"
+)
+
+const (
+	//address     = "172.17.0.2:50051"
+        address     = "localhost:50051"
+        defaultNetwork = "Bluetooth"
+        S1ProtoHead = "X1000001"
+        SNProtoHead = "XN888888"
+	defaultProtocol =  "S1"
+	defaultDemo = "protocol"
+)
+
+func Client(demotype string, demovalue string) {
+    log.Printf("%s", "myclient is running")
+
+    // Set up a connection to the server.
+    conn, err := grpc.Dial(address, grpc.WithInsecure())
+    if err != nil {
+            log.Fatalf("did not connect: %v", err)
+    }
+    defer conn.Close()
+    c := pb.NewKubedgeClient(conn)
+
+    // Contact the server and print out its response.
+    demo := defaultDemo
+    network := defaultNetwork
+    protocol := defaultProtocol
+    protoHead := S1ProtoHead
+    demo = demotype
+    if (demo == defaultDemo) {
+        protocol = demovalue
+        if protocol != defaultProtocol {
+            protoHead =  SNProtoHead
+        }
+    } else {
+        network = demovalue
+    }
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    if demo == defaultDemo {
+        r, err := c.FiveGDemo(ctx, &pb.EnodeRequest{Protocol: protoHead})
+        if err != nil {
+            log.Fatalf("Error: %v", err)
+        }
+        log.Printf("%s", r.Message)
+    } else {
+        rr, errr := c.DetectNW(ctx, &pb.UERequest{Network: network})
+        if errr != nil {
+            log.Fatalf("Error: %v", errr)
+        }
+        log.Printf("%s", rr.Message)
+    }
+    log.Printf("%s", "client is exiting")
+}
+
+
