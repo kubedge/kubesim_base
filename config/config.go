f60cd5e6c522604eb68f2d4b40810577f2e0070b
Robert Boyd <rb422w@att.com>
2018-10-14 04:40:17 +0000

    [XXXX] add integer and integer list to config, move to samples
    Change-Id: Ib8dadc5ee47b769498ea4b5c0fe91e81871f053c

--- config/config.go
+++ config/config.go
@@ -1,41 +1,33 @@
 package config
 import (
   "log"
-  //"strings"
   "gopkg.in/yaml.v2"
   "io/ioutil"
 )
 
-//type Configdata struct {
-    //Hits int64 `yaml:"hits"`
-    //Time int64 `yaml:"time"`
-//}
 
 type Configdata struct {
-  Foo string
+  Foo string `yaml:"foo"`
   Bar[] string
+  Alpha int64 `yaml:"alpha"`
+  Beta[] int64 `yaml:"beta"`
 }
 
 
 func (config *Configdata) Config() {
   log.Printf("%s", "Config invoked")
 
-  //yamlFile, err := ioutil.ReadFile("/etc/kubedge/kubesim_conf.yaml")
+  yamlFile, err := ioutil.ReadFile("/etc/kubedge/kubesim_conf.yaml")
   //yamlFile, err := ioutil.ReadFile("/tmp/kubesim_conf.yaml")
-  yamlFile, err := ioutil.ReadFile("config.yaml")
   log.Printf("yamlFile=%s", string(yamlFile))
   if err != nil {
       log.Printf("yamlFile.Get err   #%v ", err)
   }
 
-  //err = yaml.Unmarshal(yamlFile, &config)
-  //err = yaml.Unmarshal(yamlFile, config)
-  //err = yaml.Unmarshal(yamlFile, &(*config))
-  //err = yaml.Unmarshal(yamlFile, *config)
   err = yaml.Unmarshal(yamlFile, config)
   if err != nil {
       log.Fatalf("Unmarshal: %v", err)
   }
   //(*config).Foo = "hello world"    //WORKS - change seen in caller
-  log.Printf("Foo=%s, Bar=%s", config.Foo, config.Bar)
+  log.Printf("in library config:  Foo=%s, Bar=%s, Alpha=%d, Beta=%s", config.Foo, config.Bar, config.Alpha, config.Beta)
 }
