bb35924c74d83c1c46b845f0613306d6901cb0b1
Jerome Brette <jbrette@gmail.com>
2018-11-12 00:23:29 +0000

    Update server and client code to usage new messages
    Change-Id: I3e2c2fc06b57841513d2466bd3acf172534ac96f
    (cherry picked from commit eda2bcbad710c163a80905bbc37dbd37c4c59d2d)

--- grpc/go/kubedge_server/server.go
+++ grpc/go/kubedge_server/server.go
@@ -62,6 +62,174 @@ func (s *kubedgeserver) DetectNW(ctx context.Context, in *pb.UERequest) (*pb.EPC
 	return &pb.EPCReply{Message: in.Network + ":: is the mode of connection"}, nil
 }
 
+func (s *kubedgeserver) MME_PDN_CONNECT(ctx context.Context, in *pb.MME_PDN_CONNECT_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_PDN_CONNECT with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) MME_PDN_DISCONNECT(ctx context.Context, in *pb.MME_PDN_DISCONNECT_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_PDN_DISCONNECT with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) MME_ATTACH(ctx context.Context, in *pb.MME_ATTACH_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_ATTACH with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) MME_DETACH(ctx context.Context, in *pb.MME_DETACH_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_DETACH with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) MME_HO(ctx context.Context, in *pb.MME_HO_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_HO with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) MME_TAU(ctx context.Context, in *pb.MME_TAU_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_TAU with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) MME_SERVICE_REQUEST(ctx context.Context, in *pb.MME_SERVICE_REQUEST_MSG) (*pb.SIMReply, error) {
+	message := "Server received MME_SERVICE_REQUEST with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_SESSION_CREATION(ctx context.Context, in *pb.SGW_SESSION_CREATION_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_SESSION_CREATION with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_SESSION_DELETION(ctx context.Context, in *pb.SGW_SESSION_DELETION_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_SESSION_DELETION with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_BEARER_CREATION(ctx context.Context, in *pb.SGW_BEARER_CREATION_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_BEARER_CREATION with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_BEARER_DELETION(ctx context.Context, in *pb.SGW_BEARER_DELETION_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_BEARER_DELETION with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_BEARER_MODIFICATION(ctx context.Context, in *pb.SGW_BEARER_MODIFICATION_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_BEARER_MODIFICATION with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_DEDICATED_BEARER_ACTIVATE(ctx context.Context, in *pb.SGW_DEDICATED_BEARER_ACTIVATE_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_DEDICATED_BEARER_ACTIVATE with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) SGW_DEDICATED_BEARER_DEACTIVATE(ctx context.Context, in *pb.SGW_DEDICATED_BEARER_DEACTIVATE_MSG) (*pb.SIMReply, error) {
+	message := "Server received SGW_DEDICATED_BEARER_DEACTIVATE with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_RRC_CONN_SETUP(ctx context.Context, in *pb.ENB_RRC_CONN_SETUP_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_RRC_CONN_SETUP with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_S1_SIG_CONN_SETUP(ctx context.Context, in *pb.ENB_S1_SIG_CONN_SETUP_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_S1_SIG_CONN_SETUP with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_INITIAL_CTXT_SETUP(ctx context.Context, in *pb.ENB_INITIAL_CTXT_SETUP_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_INITIAL_CTXT_SETUP with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_UE_CTX_RELEASE(ctx context.Context, in *pb.ENB_UE_CTX_RELEASE_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_UE_CTX_RELEASE with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_X2HO_PREP_OUT(ctx context.Context, in *pb.ENB_X2HO_PREP_OUT_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_X2HO_PREP_OUT with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_X2HO_PREP_IN(ctx context.Context, in *pb.ENB_X2HO_PREP_IN_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_X2HO_PREP_IN with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_X2HO_EXEC_OUT(ctx context.Context, in *pb.ENB_X2HO_EXEC_OUT_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_X2HO_EXEC_OUT with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_X2HO_EXEC_IN(ctx context.Context, in *pb.ENB_X2HO_EXEC_IN_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_X2HO_EXEC_IN with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_ERAB_SETUP(ctx context.Context, in *pb.ENB_ERAB_SETUP_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_ERAB_SETUP with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
+func (s *kubedgeserver) ENB_ERAB_RELEASE(ctx context.Context, in *pb.ENB_ERAB_RELEASE_MSG) (*pb.SIMReply, error) {
+	message := "Server received ENB_ERAB_RELEASE with IMSI: " + in.Imsi
+	log.Printf(message)
+	append2log(message)
+	return &pb.SIMReply{Message: message}, nil
+}
+
 func Server(simuname string, c config.Configdata) {
 	conf = c
 	log.Printf("%s: server is running, enable_log=%s", simuname, conf.Enable_log)
