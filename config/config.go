43d950ce13bbfb3c01f8f9be5d9b621bdb8205c5
Jerome Brette <jbrette@gmail.com>
2018-11-24 15:55:59 +0000

    Move listening port and address to configuration file
    Change-Id: I0ba5d92ab83ce673187b32b64ca3a64a20c41ec2
    (cherry picked from commit b012f73adbfbb7f751fe53612ccf92a2a8522c9d)

--- config/config.go
+++ config/config.go
@@ -27,19 +27,22 @@ const DEFAULT_CONFIG_FILE = "/etc/kubedge/kubesim_conf.yaml"
 const DEBUG_LOG = false
 
 type Configdata struct {
-	Product_name    string   `yaml:"product_name"`
-	Product_type    string   `yaml:"product_type"`
-	Product_family  string   `yaml:"product_family"`
-	Product_release string   `yaml:"product_release"`
-	Feature_set1    []string `yaml:"feature_set1"`
-	Feature_set2    []string `yaml:"feature_set2"`
-	Enable_log      bool     `yaml:"enable_log"`
+	Product_name           string   `yaml:"product_name"`
+	Product_type           string   `yaml:"product_type"`
+	Product_family         string   `yaml:"product_family"`
+	Product_release        string   `yaml:"product_release"`
+	Feature_set1           []string `yaml:"feature_set1"`
+	Feature_set2           []string `yaml:"feature_set2"`
+	Enable_log             bool     `yaml:"enable_log"`
+	Kubedge_server_port    string   `yaml:"kubedge_server_port"`
+	Kubedge_server_address string   `yaml:"kubedge_server_address"`
 }
 
 func (config *Configdata) Config(simname string, filename string) {
 	if simname == "" {
 		simname = DEFAULT_SIM_NAME
 	}
+
 	if filename == "" {
 		filename = DEFAULT_CONFIG_FILE
 	}
@@ -53,6 +56,15 @@ func (config *Configdata) Config(simname string, filename string) {
 	if err != nil {
 		log.Fatalf("%s: %s: Unmarshal: %v", simname, filename, err)
 	}
+
+	if config.Kubedge_server_address == "" {
+		config.Kubedge_server_address = "192.168.2.101:30180"
+	}
+
+	if config.Kubedge_server_port == "" {
+		config.Kubedge_server_port = ":50051"
+	}
+
 	if DEBUG_LOG {
 		log.Printf("%s: %s contains: product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s, enable_log=%v",
 			simname, filename, config.Product_name, config.Product_type, config.Product_family, config.Product_release, config.Feature_set1, config.Feature_set2, config.Enable_log)
